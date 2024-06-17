e3bf841f59a0d5e98146dca08a9a51bdcf3865c3
Jason Azevedo <jasonazevedo03@gmail.com>
2024-06-17 08:26:46 +0200

    Moved the text input to be inline with the label

--- src/google-chrome/js/main.js
+++ src/google-chrome/js/main.js
@@ -21,7 +21,22 @@ export default class YTMusicQuiet {
    * Register listeners for the UI elements.
    */
   registerListeners() {
-    // TODO: Implement listeners
+    const scalerNumberInput = document.getElementById("numberScaleInput");
+    const scalerSliderInput = document.getElementById("sliderScaleInput");
+
+    scalerNumberInput.addEventListener("change", (e) => {
+      // Handle the new input.
+      console.log(`Text input: ${e.target.value}`);
+
+      // Call a method to automagically scale the volume.
+    });
+
+    scalerSliderInput.addEventListener("input", (e) => {
+      // Handle the slider input.
+      scalerNumberInput.value = e.target.value;
+
+      // Call a method to automagically scale the volume.
+    });
   }
 }
 
